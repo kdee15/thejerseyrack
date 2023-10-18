be504836beb9d092c7a32fbe9363ea0ccdf857b5
Kirk Daniels <kirkd@ayoba.me>
2023-10-18 21:07:14 +0200

    Update contentful-config.js

--- helpers/contentful-config.js
+++ helpers/contentful-config.js
@@ -1,5 +1,5 @@
-const C_SPACE_ID = process.env.CONTENTFUL_SPACE_ID;
-const C_DELIVERY_KEY = process.env.CONTENTFUL_DELIVERY_KEY;
+const C_SPACE_ID = process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID;
+const C_DELIVERY_KEY = process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN;
 const C_GRAPHQL_URL = `https://graphql.contentful.com/content/v1/spaces/${C_SPACE_ID}/environments/master`;
 
 module.exports = {
